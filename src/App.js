893cb1e366bede0152bb7feb1f16a430fe1d8882
FatimaBartels <bartels.fatima@gmail.com>
2024-09-24 10:46:26 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./site-logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="App-logo" />
+      </header>
+    </div>
+  );
+}
