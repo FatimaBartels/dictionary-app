579238f8ca79eed7db89ff226ed0a226de9ebac9
FatimaBartels <bartels.fatima@gmail.com>
2024-09-25 13:22:18 +0200

    added form and submit function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./site-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="App-logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="App-logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Fatima Bartels</footer>
+      </div>
     </div>
   );
 }
